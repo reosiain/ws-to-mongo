951e016af4d65e80b7480325fd96c4a4009db32a
reosiain <54156926+reosiain@users.noreply.github.com>
2022-05-05 13:30:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -35,29 +35,34 @@ void run_pusher(std::string db_url, std::vector<json>* input_container, std::vec
 mongocxx::instance MongoPusher::inst = mongocxx::instance{};
 
 int main() {
+        
+    try{       
+        std::string url = std::getenv("WS_ADDRESS");
+        std::string db_url = std::getenv("MONGO_URI");
+        std::string params_path = std::getenv("SUBSCRIPTION_PARAMS");
 
-    std::string url = std::getenv("WS_ADDRESS");
-    std::string db_url = std::getenv("MONGO_URI");
-    std::string params_path = std::getenv("SUBSCRIPTION_PARAMS");
 
+        std::ifstream ifs(params_path.c_str());
+        json j = json::parse(ifs);
+        std::string params = j.dump();
 
-    std::ifstream ifs(params_path.c_str());
-    json j = json::parse(ifs);
-    std::string params = j.dump();
+        std::vector<json> input_container;
+        std::vector<json> output_container;
 
-    std::vector<json> input_container;
-    std::vector<json> output_container;
+        std::vector<std::thread> threads;
+        std::thread thread_ws(run_ws, url, params, &input_container);
+        std::thread thread_pu(run_pusher, db_url, &input_container, &output_container);
 
-    std::vector<std::thread> threads;
-    std::thread thread_ws(run_ws, url, params, &input_container);
-    std::thread thread_pu(run_pusher, db_url, &input_container, &output_container);
+        threads.push_back(std::move(thread_ws));
+        threads.push_back(std::move(thread_pu));
 
-    threads.push_back(std::move(thread_ws));
-    threads.push_back(std::move(thread_pu));
-
-    for (auto& t : threads) {
-        t.join();
-        std::cout << "Thread started" << std::endl;
+        for (auto& t : threads) {
+            t.join();
+            std::cout << "Thread started" << std::endl;
+        }
+    }catch(...) {
+        std::cout << "Unexpected error in main thread" << std::endl;
+        std::cout << boost::stacktrace::stacktrace();
     }
 
     return 0;
